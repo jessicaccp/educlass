356595ac978facfe356ee9b2b14298123b713491
Jessica Cacau <jessicacacau@gmail.com>
2022-12-04 23:06:47 -0300

    Arruma css e adiciona tela de início

--- config.py
+++ config.py
@@ -22,6 +22,6 @@ class Config(object):
     MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
     REDIS_URL = os.environ.get('REDIS_URL') or 'redis://'
-    POSTS_PER_PAGE = 25
+    POSTS_PER_PAGE = 5
     CORS_HEADERS = 'application/json'
 
